d0ec6d3fe7ce94403844d93145610c6e4272f389
estyhellerwd2017 <qwertsy2@gmail.com>
2017-02-09 12:36:33 +0200

    ajax

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 $(function(){
-    $("#contact_form").submit(){
+    var $theForm = $("#contact_form");
+    $theForm.submit(function(e){
         e.preventDefault();
-    };
+        var form_data = $theForm.serialize();
+
+        $.ajax(
+            {type: 'POST', url: $theForm.attr("action"), data: form_data}
+        ).done(function(response){
+            $theForm.html("<h2>"+response+"</h2>");
+        }).fail(function(response){
+            $theForm.append("<h2>"+response+"</h2>");
+        });
+    });
+
+
 });
